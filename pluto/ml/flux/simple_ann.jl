b752e89e6ea2bea14c69d9a33e3373da86f8ca1f
TEMBOAN <temboan@zra.org.zm>
2022-08-03 17:01:57 +0200

    Implement simple ANN

---
+++ pluto/ml/flux/simple_ann.jl
@@ -0,0 +1,53 @@
+using Plots, Flux
+
+#function to multiply 3 numbers
+addFunc(X) = X[1] + X[2] + X[3]
+
+getRandon3Numbers() = rand(Int16,3) ./1000
+
+#Generate training data samples
+x_train = []
+
+for i in 1:15000
+    push!(x_train,getRandon3Numbers())
+end
+
+x_train = (hcat(x_train...))
+y_train_vec = []
+
+for col in eachcol(x_train)
+    push!(y_train_vec,addFunc(col))
+end
+
+y_train = hcat(y_train_vec...)
+
+#model
+model = Dense(3=>1)
+
+ps=Flux.params(model)
+
+loss(x,y) = Flux.Losses.mse(model(x), y)
+
+loss(x_train, y_train)
+
+opt=Adam()
+
+data=[(x_train, y_train)]
+
+Flux.@epochs 5000 Flux.train!(loss,ps,data,opt)
+
+#Generate test data
+x_test =[]
+for i in 1:5
+    push!(x_test,getRandon3Numbers())
+end
+
+#test model
+for x in x_test
+    @show x
+    @show model(x)
+    println("-----------------------")
+end
+
+
+model([3000,1000,1000])
